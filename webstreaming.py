08560655d2efbf95a4bfb90a37ca6ead82d5e8e9
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-09 15:06:53 +0900

    add multithreading mode in flask

--- webstreaming.py
+++ webstreaming.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, Response, request
 import requests
 import cv2
 import numpy as np
-from .models.detect import detect
-#from .models.MaskDetection.detect import detection
 
 
 app = Flask(__name__)
@@ -16,23 +14,41 @@ def index():
     client_ip = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
     return render_template("index.html")
 
+def generate(tag_id):
+    # General OpenCV Mode
+    if tag_id == "general":
+        while True:
+            try:
+                res = requests.get("http://{}:5000/".format(client_ip)).content
+                # OpenCV Bbox
+                yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
+            except Exception as err:
+                print("ERROR MSG:[{}]".format(err))
+    # Cycle GAN Mode
+    elif tag_id == "Gan":
+        while True:
+            try:
+                res = requests.get("http://{}:5000/".format(client_ip)).content
+                # Adapt Cycle GAN
+                yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
+            except Exception as err:
+                print("ERROR MSG:[{}]".format(err))
+    # template mode
+    elif tag_id == "template":
+        while True:
+            try:
+                res = requests.get("http://{}:5000/".format(client_ip)).content
+                # Adapt template Mode
+                yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
+            except Exception as err:
+                print("ERROR MSG:[{}]".format(err))
 
-def gen():
-    while True:
-        try:
-            res = requests.get("http://{}:5000/".format(client_ip)).content
-            # Mask detection & convention
-            result = detect(res)
-            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
-        except Exception as err:
-            print("ERROR MSG:[{}]".format(err))
 
-
-@app.route("/video_feed")
-def video_feed():
+@app.route("/video_feed/<string:id>")
+def video_feed(id):
     # 요청을 보냈던 client의 Ip로 이미지 수신
-    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(generate(id), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
