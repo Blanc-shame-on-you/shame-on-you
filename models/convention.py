e42c2bf1fa9827b639a0cdb92494547ee7ed7fee
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-10 11:09:02 +0900

    add image conventino code Resnet, convention.py

--- models/convention.py
+++ models/convention.py
@@ -1,27 +1,34 @@
 import os
 import cv2 
 import numpy 
+import os
+import cv2 
 
-def bitOperation(directions, mainImg):
-    addImage = cv2.imread('./virus.png')
-    
-    addImggray = cv2.cvtColor(addImg, cv2.COLOR_BGR2GRAY)
-    ret, mask = cv2.threshold(addImggray, 10, 255, cv2.THRESH_BINARY)
+def bitOperation(locations,img1):
+  img2 = cv2.imread(os.getcwd().replace('\\','/')+'/models/virus.png')
+  for x2,y2,x1,y1 in locations:
+    if x2-x1 > y2-y1:
+      img2 = cv2.resize(img2, dsize=(x2-x1, x2-x1), interpolation=cv2.INTER_AREA)
+      #img2 = cv2.resize(img2, dsize=(x2-x1, x2-x1), interpolation=cv2.INTER_AREA)
+    else:
+      img2 = cv2.resize(img2, dsize=(y2-y1, y2-y1), interpolation=cv2.INTER_AREA)
+
+    img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
-    addImg_fg = cv2.bitwise_and(mainImg, addImg, mask=mask)
+    img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
 
-    rows, cols = addImg.shape[:2]
-    for vpos,hpos in directions:
-        roi = mainImg[vpos:rows+vpos, hpos:cols+hpos]
-        #roi = mainImg[vpos:rows+vpos, hpos:cols+hpos]
-        
-        mainImg_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
-        #mainImg_bg = cv2.bitwise_and(roi,roi, mask=mask_inv)
+    rows, cols = img2.shape[:2] # 바이러스 이미지의 가로, 세로 
 
-        dst = cv2.add(mainImg_bg, addImg_fg)
-        mainImg[vpos:rows+vpos, hpos:cols+hpos] = dst
-        #mainImg[vpos:rows+vpos, hpos:cols+hpos].copyTo(addImg_fg)
+    vpos = x1
+    hpos = y1    
 
-    ret, result = cv2.imencode('.jpg', mainImg)
-    result = result.tobytes()
-    return result
+    roi = img1[vpos:rows+vpos, hpos:cols+hpos]
+
+    img1_bg = cv2.bitwise_and(roi,roi, mask=mask_inv)
+
+
+    dst = cv2.add(img1_bg, img2_fg)
+    img1[vpos:rows+vpos, hpos:cols+hpos] = dst    
+
+  return img1
