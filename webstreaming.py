ee3869b4283d383f295d6811f56733bc18241986
gpusystem <woungsub1234@gmail.com>
2020-11-16 23:04:32 +0000

    FIX frame transferring error

--- webstreaming.py
+++ webstreaming.py
@@ -6,24 +6,32 @@ from threading import Thread
 import sqlite3
 from models.main import mask_detection, detect, original, cycleGan
 
+CAM_IP = '10.120.73.197'
+rtsp_url = "rtsp://{}:8554/live/stream".format(CAM_IP)
+frame = None
 app = Flask(__name__)
+#global frame
+
+def get_frame():
+    global frame
+    cap=cv2.VideoCapture(rtsp_url)
+    while cap.isOpened():
+        _,frame = cap.read()
+
 
 @app.route("/")
-def index():
-    global rtsp_url
-    # 접속한 라즈베리파이의 IP 저장
-    client_ip = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
-    rtsp_url = "rtsp://{}:8554/live/stream".format(rtsp_url)
+def index(): 
     return render_template("index.html")
 
 def generate(tag_id):
     # General OpenCV Mode
+    #global frame
+
     if tag_id == "general":
         while True:
             try:
-                res = cv2.VideoCapture(rtsp_url)
-                decoded = cv2.imdecode(np.frombuffer(res, np.uint8), -1)
-                result = original(decoded)
+                #decoded = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
+                result = original(frame)
                 res = cv2.imencode('.jpg', result)[1].tobytes()
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
             except Exception as err:
@@ -32,9 +40,8 @@ def generate(tag_id):
     elif tag_id == "GAN":
         while True:
             try:
-                res = cv2.VideoCapture(rtsp_url)
-                decoded = cv2.imdecode(np.frombuffer(res, np.uint8), -1)
-                result = cycleGan(decoded)
+            #ecoded = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
+                result = cycleGan(frame)
                 res = cv2.imencode('.jpg', result)[1].tobytes()
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
 
@@ -42,13 +49,10 @@ def generate(tag_id):
                 print("ERROR MSG = [{}]".format(err))
     # template mode
     elif tag_id == "template":
-
         while True:
-
-            try:                
-                res = cv2.VideoCapture(rtsp_url)
-                decoded = cv2.imdecode(np.frombuffer(res, np.uint8), -1)
-                result = detect(decoded)
+            try:
+            #decoded = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
+                result = detect(frame)
                 res = cv2.imencode('.jpg', result)[1].tobytes()
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + res + b"\r\n")
             except Exception as err:
@@ -58,15 +62,18 @@ def generate(tag_id):
 @app.route("/video_feed/<string:tag_id>")
 def video_feed(tag_id):
     # 요청을 보냈던 client의 Ip로 이미지 수신
+    #global cap
+    #cap = cv2.VideoCapture(rtsp_url)
+    y=Thread(target=get_frame,args=()).start()
     x=Thread(target=generate,args=(tag_id,)).start()
     return Response(generate(tag_id), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-@app.route('/getData'):
+@app.route('/getData')
 def get_people_data():
     conn = sqlite3.connect("people.db")
 
-    cur = conn.cursor():
+    cur = conn.cursor()
     cur.execute("SELECT * FROM people ORDER BY time")
     rows = cur.fetchall()
     cur.close()
